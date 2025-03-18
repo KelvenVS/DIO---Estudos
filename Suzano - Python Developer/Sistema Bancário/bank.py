a9cc7bb0f1d1cb1bcb0830b1a4ac39bbfdbf8210
KelvenVS <kelven.serejo@gmail.com>
2025-03-18 13:39:14 -0300

    🔒 feat: Adicionado limite de transações diárias e reformulado extrato
    - Implementado controle de limite de transações por dia.
    - Alterada estrutura do extrato de lista para dicionário, melhorando a organização dos dados.

--- Suzano - Python Developer/Sistema Bancário/bank.py
+++ Suzano - Python Developer/Sistema Bancário/bank.py
@@ -4,8 +4,21 @@ def menu():
     saldo = 0
     limite_por_saque = 500
     saques_por_dia = 3
-    extrato = []
+    trans_por_dia = 10
     
+    extrato = {
+        "Operação": [],
+        "Data/Hora": [],
+        "Saldo Anterior": [],
+        "Valor": [],
+        "Saldo Atual": []
+        }
+        
+    def registrar_extrato(data):
+        data = list(data)
+        for key,value in zip(extrato,data):
+            extrato[key].append(value)       
+        
     def date_now():
         d = datetime.datetime.now()
         return d.strftime("%d/%m/%Y %H:%M")
@@ -14,13 +27,19 @@ def menu():
         return (f'R$ {value:.2f}')
     
     def option_visualizar_extrato():
-        nonlocal extrato
-        if extrato == []:
-            print(f'Não Foram realizadas movimentações')
+        if not extrato["Operação"]:
+            print(f"Não Foram realizadas movimentações")
         else:
-            print(f"{'Operação':<10}{'Data/Hora':>20}{'Saldo Anterior':>15}{'Valor':>15}{'Saldo Atual':>15}")
-            for operacao,data_hora,valor,saldo_anterior,saldo in extrato:
-                print(f'{operacao:<10}{data_hora:>20}{format_value(saldo_anterior):>15}{format_value(valor):>15} {format_value(saldo):>15}')
+            print(f"{'Operação':<15}{'Data/Hora':<20}{'Saldo Anterior':>15}{'Valor':>15}{'Saldo Atual':>15}")
+            
+            for operacao, data_hora, saldo_anterior, valor, saldo in zip(
+                extrato["Operação"], 
+                extrato["Data/Hora"], 
+                extrato["Saldo Anterior"], 
+                extrato["Valor"], 
+                extrato["Saldo Atual"]
+            ):
+                print(f"{operacao:<15}{data_hora:<20}{format_value(saldo_anterior):>15}{format_value(valor):>15}{format_value(saldo):>15}")
 
     def option_visualizar_saldo():
         print(f'O saldo atual é de {format_value(saldo)}')
@@ -31,36 +50,44 @@ def menu():
     
     def option_sacar():
         nonlocal saques_por_dia
+        nonlocal trans_por_dia
         nonlocal saldo
-        saldo_anterior = saldo
         nonlocal option
+        saldo_anterior = saldo
         
         valor = int(input('Digite o valor do saque: R$'))
         
-        if saques_por_dia <= 0:
-            print(f'Limite de saque diário ultrapassado. Tente novamente mais tarde.')
+        if saques_por_dia <= 0 or trans_por_dia <= 0:
+            print(f'O limite diário de saques ou transações foi excedido. Por favor, tente novamente mais tarde.')
         elif valor > saldo:
             print(f'Não é possivel sacar o valor {format_value(valor)}.')
             option_visualizar_saldo()
         elif valor > limite_por_saque:
-            print(f'Não é possivel sacar o valor {format_value(valor)} , pois é maior que limite por saque / {format_value(limite_por_saque)}')
+            print(f'Não é possível sacar o valor {format_value(valor)}, pois excede o limite permitido por saque: {format_value(limite_por_saque)}.')
         else:
-            print(f'Sacando {format_value(valor)} de {format_value(saldo)}')
+            print(f'Realizando o saque de {format_value(valor)}')
             saldo-=valor
             saques_por_dia-=1
-            extrato.append((menu_keys[option - 1],date_now(),valor,saldo_anterior,saldo))
+            trans_por_dia-=1
+            data = (menu_keys[option - 1],date_now(),saldo_anterior,valor,saldo)
+            registrar_extrato(data)
             option_visualizar_saldo()
             
     def option_depositar():
         nonlocal saldo
         saldo_anterior = saldo
         nonlocal option
-        
-        valor = int(input('Digite o valor do depósito: R$'))
-        print(f'Depositando {format_value(valor)}')
-        saldo+=valor
-        extrato.append((menu_keys[option - 1],date_now(),valor,saldo_anterior,saldo))
-        option_visualizar_saldo()
+        nonlocal trans_por_dia
+        if trans_por_dia <= 0:
+            print('O limite diário transações foi excedido')
+        else:
+            valor = int(input('Digite o valor do depósito: R$'))
+            print(f'Depositando {format_value(valor)}')
+            saldo+=valor
+            trans_por_dia-=1
+            data = (menu_keys[option - 1],date_now(),saldo_anterior,valor,saldo)
+            registrar_extrato(data)
+            option_visualizar_saldo()
     
     menu_dict = {
         'Sacar': option_sacar,
@@ -73,12 +100,12 @@ def menu():
     menu_keys = list(menu_dict.keys())
     
     while True:
-        print(f"{'#'*25} Menu {'#'*25}")
+        print(f"{'#'*47} Menu {'#'*47}")
         
         for i, elem in enumerate(menu_keys, 1):
             print(f"{i}. {elem}")
         
-        print(f"{'#'*56}") 
+        print(f"{'#'*100}") 
         option = int(input(f'Escolha uma das opções: '))
 
         if option not in range(1,len(menu_keys) + 1):
