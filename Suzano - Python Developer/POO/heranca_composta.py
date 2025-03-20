25907ea80bd7a80ac120212d15b4aab114b7a94e
KelvenVS <kelven.serejo@gmail.com>
2025-03-20 11:54:45 -0300

    🦆 feat: Implementação de herança múltipla com Ornitorrinco
    - Criadas classes `Animal`, `Mamifero` e `Ave` para herança simples.
    - Implementada herança múltipla na classe `Ornitorrinco`, herdando de `Mamifero` e `Ave`.
    - Utilizado `super()` e chamadas diretas para gerenciamento de construtores.
    - Aplicado polimorfismo nos métodos `voa()` e `amamenta()`.
    - Personalizada representação de objetos com `__str__` para melhor depuração.
    - Adicionado método dinâmico `class_name` para acessar o nome da classe.

--- Suzano - Python Developer/POO/heranca_composta.py
+++ Suzano - Python Developer/POO/heranca_composta.py
@@ -7,6 +7,9 @@ class Animal:
     def __str__(self):
         return f"{self.__class__.__name__}: {', '.join([f'{chave.capitalize()}:{valor}' for chave,valor in self.__dict__.items()])}"
 
+    def class_name(self):
+        return self.__class__.__name__
+    
 class Mamifero(Animal):
     def __init__(self, num_patas, peso, tamanho):
         super().__init__(num_patas, peso, tamanho)
@@ -36,27 +39,25 @@ class Gato(Mamifero):
 class Leao(Mamifero):
     pass
 
-class Ornintorrinco(Ave,Mamifero):
-    def __init__(self,nome, cor, num_patas, peso, tamanho):
+class Ornintorrinco(Ave, Mamifero):
+    def __init__(self, nome, cor, num_patas, peso, tamanho):
         self.nome = nome
         self.cor = cor
-        super().__init__(num_patas, peso, tamanho)
-        
+        Ave.__init__(self, num_patas, peso, tamanho)
+        Mamifero.__init__(self, num_patas, peso, tamanho)
+
     def voa(self):
         return False
-    
-    def class_name(self):
-        return self.__class__.__name__
         
 if __name__ == "__main__":
     ## Herança Simples Cachorro
     dog1 = Cachorro("Ralf", "Preto e Bege", 4, 10, "25cm")
     print(dog1)
-    print("Amamenta" if dog1.amamenta() else "Não amamenta")
+    print(f"{dog1.class_name()} amamenta" if dog1.amamenta() else "Não amamenta")
 
     ## Herança Composta Ornitorrinco
     orn1 = Ornintorrinco("Pedro", "Azul-Escuro", 4, 15, "30cm")
     print(orn1)
-    print("Amamenta os filhotes" if orn1.amamenta() else "Não amamenta os filhotes")
-    print(f"{orn1.class_name()} bota ovos" if orn1.voa() else "Ele não voa")
-    print("Bota ovos" if orn1.bota_ovo() else "Não bota ovos")
+    print(f"{orn1.class_name()} amamenta os filhotes" if orn1.amamenta() else f"{orn1.class_name()} não amamenta os filhotes")
+    print(f"{orn1.class_name()} voa" if orn1.voa() else f"{orn1.class_name()} não voa")
+    print(f"{orn1.class_name()} bota ovos" if orn1.bota_ovo() else f"{orn1.class_name()} Não bota ovos")
